8b88a5fa5ab5584240ba0718bd3e5c5e70596233
rmuraix <35632215+rmuraix@users.noreply.github.com>
2023-09-30 03:52:32 +0900

    Add formatter and linter

--- src/functions/pilline.ts
+++ src/functions/pilline.ts
@@ -1,15 +1,23 @@
-import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
+import {
+  app,
+  HttpRequest,
+  HttpResponseInit,
+  InvocationContext,
+} from "@azure/functions";
 
-export async function pilline(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
-    context.log(`Http function processed request for url "${request.url}"`);
+export async function pilline(
+  request: HttpRequest,
+  context: InvocationContext,
+): Promise<HttpResponseInit> {
+  context.log(`Http function processed request for url "${request.url}"`);
 
-    const name = request.query.get('name') || await request.text() || 'world';
+  const name = request.query.get("name") || (await request.text()) || "world";
 
-    return { body: `Hello, ${name}!` };
-};
+  return { body: `Hello, ${name}!` };
+}
 
-app.http('pilline', {
-    methods: ['GET', 'POST'],
-    authLevel: 'anonymous',
-    handler: pilline
+app.http("pilline", {
+  methods: ["GET", "POST"],
+  authLevel: "anonymous",
+  handler: pilline,
 });
