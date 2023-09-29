434e2c0e328b439a4efbfa013d93bd63fbf03b4d
rmuraix <35632215+rmuraix@users.noreply.github.com>
2023-09-30 03:33:10 +0900

    Initial commit

---
+++ src/functions/pilline.ts
@@ -0,0 +1,15 @@
+import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
+
+export async function pilline(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
+    context.log(`Http function processed request for url "${request.url}"`);
+
+    const name = request.query.get('name') || await request.text() || 'world';
+
+    return { body: `Hello, ${name}!` };
+};
+
+app.http('pilline', {
+    methods: ['GET', 'POST'],
+    authLevel: 'anonymous',
+    handler: pilline
+});
